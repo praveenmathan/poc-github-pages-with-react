be4ed2aa218dde213e504e36676e9e938ea87c24
praveen <praveen@praveens-MacBook-Pro.local>
2020-05-07 18:38:33 +0530

    Updates

---
+++ precache-manifest.6c3e37c132599b41d5e343244464fa04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f731e60cbe4b1c385bfd4bc3c4526f2b",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "5804ce1c3a6fbcb73ec6",
+    "url": "/myreact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1858b4fed0a4f8aaf5ed",
+    "url": "/myreact/static/js/2.720e45b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreact/static/js/2.720e45b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5804ce1c3a6fbcb73ec6",
+    "url": "/myreact/static/js/main.d682731c.chunk.js"
+  },
+  {
+    "revision": "0a3f1639ae361d118186",
+    "url": "/myreact/static/js/runtime-main.2c929bdb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
